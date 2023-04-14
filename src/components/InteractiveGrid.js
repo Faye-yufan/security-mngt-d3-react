b48dd4b247b5e521277008b7171755e290d4e697
eskimo <1145400536@qq.com>
2023-04-14 21:24:04 +0800

    feat modify structure

--- src/components/InteractiveGrid.js
+++ src/components/InteractiveGrid.js
@@ -5,6 +5,16 @@ import secondFloorImageUrl from "../asset/floor-2-first.png";
 import thirdFloorImageUrl from "../asset/floor-3-second.png";
 import fourthFloorImageUrl from "../asset/floor-4-third.png";
 import { debounce } from "../utils/utils.js";
+import {
+  createImageGroup,
+  createOverlayGroup,
+  createGripGroup,
+  createAssignedCells,
+  plotDataPoint,
+  handleAreaSelect,
+  drawCurData,
+  TIME_GAP
+} from "./interactiveGridMethods";
 
 const InteractiveGrid = ({
   dataPoints,
@@ -21,7 +31,24 @@ const InteractiveGrid = ({
   const [currentDataPoints, setCurrentDataPoints] = useState([]);
   const startPlottingRef = useRef(startPlotting);
   const svgRef = useRef();
-  let updatedSelectedCells = [...selectedCells];
+  const [updatedSelectedCells, setUpdatedSelectedCells] = useState([
+    ...selectedCells,
+  ]);
+
+  const backgroundImageUrl = () => {
+    switch (selectedOption) {
+      case "Ground Floor":
+        return firstFloorImageUrl;
+      case "1st Floor":
+        return secondFloorImageUrl;
+      case "2nd Floor":
+        return thirdFloorImageUrl;
+      case "3rd Floor":
+        return fourthFloorImageUrl;
+      default:
+        return firstFloorImageUrl;
+    }
+  };
 
   useEffect(() => {
     const width = 600;
@@ -38,275 +65,34 @@ const InteractiveGrid = ({
     const yScale = d3.scaleLinear().domain([0, gridSize]).range([0, height]);
 
     // Create a group for the image
-    const imageGroup = svg.append("g");
+    createImageGroup(svg, width, height, backgroundImageUrl);
 
     // Create a group for the overlay layer
-    const overlayGroup = svg.append("g");
-
-    const backgroundImageUrl = () => {
-      switch (selectedOption) {
-        case "Ground Floor":
-          return firstFloorImageUrl;
-        case "1st Floor":
-          return secondFloorImageUrl;
-        case "2nd Floor":
-          return thirdFloorImageUrl;
-        case "3rd Floor":
-          return fourthFloorImageUrl;
-        default:
-          return firstFloorImageUrl;
-      }
-    };
-
-    imageGroup
-      .append("image")
-      .attr("xlink:href", backgroundImageUrl)
-      .attr("x", 0)
-      .attr("y", 0)
-      .attr("width", width)
-      .attr("height", height)
-      .attr("pointer-events", "none");
-    // Reorder the groups so that the overlay group is between the image group and the grid group
-    imageGroup.raise(); // Bring the image group to the top
-    overlayGroup.raise(); // Bring the overlay group to the top (above the image group)
-
-    // Add a semi-transparent white rectangle to the overlay group
-    overlayGroup
-      .append("rect")
-      .attr("x", 0)
-      .attr("y", 0)
-      .attr("width", width)
-      .attr("height", height)
-      .attr("fill", "white")
-      .attr("opacity", 0.5);
+    createOverlayGroup(svg, width, height);
 
     // Create a group for the grid lines
-    const gridGroup = svg.append("g");
+    createGripGroup({ svg, width, height, xScale, yScale, gridSize });
 
     // Create a group for the rectangles
     const rectGroup = svg.append("g");
-
-    const drawGrid = () => {
-      gridGroup.selectAll(".grid-line").remove();
-
-      const gridLines = d3.range(0, gridSize + 1);
-
-      // Vertical lines
-      gridGroup
-        .selectAll(".grid-line-vertical")
-        .data(gridLines)
-        .enter()
-        .append("line")
-        .attr("class", "grid-line grid-line-vertical")
-        .attr("x1", (d) => xScale(d))
-        .attr("x2", (d) => xScale(d))
-        .attr("y1", 0)
-        .attr("y2", height)
-        .attr("stroke", "grey")
-        .attr("opacity", 0.6)
-        .attr("stroke-width", 1);
-
-      // Horizontal lines
-      gridGroup
-        .selectAll(".grid-line-horizontal")
-        .data(gridLines)
-        .enter()
-        .append("line")
-        .attr("class", "grid-line grid-line-horizontal")
-        .attr("x1", 0)
-        .attr("x2", width)
-        .attr("y1", (d) => yScale(d))
-        .attr("y2", (d) => yScale(d))
-        .attr("stroke", "grey")
-        .attr("opacity", 0.6)
-        .attr("stroke-width", 1);
-    };
-
-    drawGrid();
-
-    // render the assigned cells first
-    const renderAssignedCells = () => {
-      assignments.forEach((assignment) => {
-        const zoneName = assignment.zoneName;
-
-        let firstCellInAssignment;
-        assignment.selectedCells.forEach((cell, index) => {
-          const cellId = `cell-${cell.x}-${cell.y}`;
-          const existingCell = rectGroup.select(`#${cellId}`);
-
-          if (existingCell.empty()) {
-            rectGroup
-              .append("rect")
-              .attr("id", cellId)
-              .attr("x", xScale(cell.x))
-              .attr("y", yScale(cell.y))
-              .attr("width", xScale(1) - xScale(0))
-              .attr("height", yScale(1) - yScale(0))
-              .attr("fill", "#CBC568")
-              .attr("opacity", 0.5);
-          }
-
-          if (index === 0) {
-            firstCellInAssignment = cell;
-          }
-        });
-
-        // Add the surrounding square
-        rectGroup
-          .append("rect")
-          .attr("class", "zone-name-tag-bg")
-          .attr("x", xScale(firstCellInAssignment.x) + xScale(1) + 2 - 2) // Adjust the position as needed
-          .attr("y", yScale(firstCellInAssignment.y) - 20) // Adjust the position as needed
-          .attr("width", zoneName.length * 6 * 2) // Adjust the width based on the length of the zone name
-          .attr("height", 25) // Adjust the height as needed
-          .attr("fill", "#DCDCDC")
-          .attr("opacity", 0.8);
-
-        // Add the zone name tag
-        rectGroup
-          .append("text")
-          .attr("class", "zone-name-tag")
-          .attr("x", xScale(firstCellInAssignment.x) + xScale(1) + 2) // Adjust the position as needed
-          .attr("y", yScale(firstCellInAssignment.y) - 2) // Adjust the position as needed
-          .attr("font-size", "24px")
-          .attr("font-weight", "bold")
-          .attr("fill", "black")
-          .attr("opacity", 0.8)
-          .text(zoneName);
-      });
-    };
-
-    renderAssignedCells();
-
-    // plot the current data points
-    const plotDataPoint = (rectGroup, dataPoint, deviceColors) => {
-      const x = dataPoint.projected_norm_x;
-      const y = dataPoint.projected_norm_y;
-      const isInDeviceColors = deviceColors[dataPoint.ClientMacAddr];
-      const staffOrDevice = dataPoint["Staff/Device"];
-
-      // Selected fixed device but the state has not occurred yet
-      const isFixedNormal =
-        isInDeviceColors && dataPoint.localtime < 1568121986000;
-      // Selected fixed device and the state has occurred
-      const isFixedWithState =
-        isInDeviceColors && dataPoint.localtime >= 1568121986000;
-      // is staff but not specifically for this device
-      const isStaff = dataPoint["Staff ID"] !== "nan";
-
-      let color, size, alpha, strokeWidth, stroke;
-
-      if (isFixedNormal) {
-        color = "#0096FF";
-        size = 8;
-        alpha = 1;
-        strokeWidth = 3;
-        stroke = "black";
-      } else if (isFixedWithState) {
-        color = "red";
-        size = 10;
-        alpha = 1;
-        strokeWidth = 2;
-        stroke = "black";
-      } else if (isStaff) {
-        color = "orange";
-        size = 7;
-        alpha = 1;
-        strokeWidth = 0;
-        stroke = "transparent";
-      } else {
-        // Default case
-        color = "black";
-        size = 5;
-        alpha = 0.5;
-        strokeWidth = 0;
-        stroke = "transparent";
-      }
-
-      // Show tooltip
-      const showTooltip = (event) => {
-        const tooltip = document.getElementById("tooltip");
-        const content = `
-          <strong>${staffOrDevice}</strong><br/>
-          ClientMacAddr: ${dataPoint.ClientMacAddr}`;
-
-        tooltip.innerHTML = content;
-        tooltip.style.left = event.pageX + 10 + "px";
-        tooltip.style.top = event.pageY + 10 + "px";
-        tooltip.style.display = "block";
-      };
-
-      // Hide tooltip
-      const hideTooltip = () => {
-        const tooltip = document.getElementById("tooltip");
-        tooltip.style.display = "none";
-      };
-
-      // Plot the data point
-      rectGroup
-        .append("circle")
-        .attr("cx", x)
-        .attr("cy", height - y)
-        .attr("r", size)
-        .attr("fill", color)
-        .attr("opacity", alpha)
-        .attr("pointer-events", "all")
-        .style("stroke", stroke)
-        .style("stroke-width", strokeWidth)
-        .on("mouseover", showTooltip)
-        .on("mousemove", showTooltip)
-        .on("mouseout", hideTooltip);
-    };
+    createAssignedCells({ rectGroup, svg, assignments, xScale, yScale });
 
     const startPlottingDataPoints = (currentDataIndex) => {
-      const startTime = dataPoints[currentDataIndex + 1].localtime; // get the start time of the data
-      const timeDiff = 500; // set the time interval to plot data
       let currentIndex = currentDataIndex;
-
       const plotDataPoints = () => {
         if (!startPlottingRef.current) {
           return; // If the "Stop" button is clicked, stop the function execution
         }
-
-        // clear previous data points
-        rectGroup.selectAll("circle").remove();
-
-        // get the current time window
-        const currentTime = startTime + currentIndex * timeDiff;
-        const currentData = dataPoints.filter(
-          (d) =>
-            d.localtime >= currentTime && d.localtime < currentTime + timeDiff
-        );
-        setCurrentDataPoints((prevCurrentDataPoints) => currentData);
-
-        // Check if the current time is 13:26:26
-        if (
-          currentData.length !== 0 &&
-          currentData[0].localtime === 1568121986000
-        ) {
-          onDataPointsForTime(currentData, assignments);
-        }
-
-        currentData.forEach((dataPoint) => {
-          plotDataPoint(rectGroup, dataPoint, deviceColors);
-          // Update the timer display
-          const timerDisplay = document.getElementById("timer");
-          const date = new Date(dataPoint.localtime);
-          const londonTimeOptions = {
-            timeZone: "UTC",
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
-          };
-          const londonTime = date.toLocaleString("en-GB", londonTimeOptions);
-          const fractionalSeconds = date.getMilliseconds();
-
-          timerDisplay.textContent = `${londonTime}.${
-            fractionalSeconds ? "5" : "0"
-          }`;
+        // draw function
+        drawCurData({
+          currentIndex,
+          rectGroup,
+          dataPoints,
+          setCurrentDataPoints,
+          onDataPointsForTime,
+          assignments,
+          deviceColors,
+          height,
         });
 
         currentIndex++;
@@ -314,7 +100,7 @@ const InteractiveGrid = ({
 
         // set timeout to plot the next data points
         if (currentIndex < dataPoints.length) {
-          setTimeout(plotDataPoints, timeDiff);
+          setTimeout(plotDataPoints, TIME_GAP);
         }
       };
 
@@ -330,52 +116,26 @@ const InteractiveGrid = ({
     }
 
     svg.on("click", (event) => {
-      const coords = d3.pointer(event);
-
-      const x = Math.floor(xScale.invert(coords[0]));
-      const y = Math.floor(yScale.invert(coords[1]));
-
-      const cellId = `cell-${x}-${y}`;
-      const assignedCells = assignments
-        .map((assignment) => assignment.selectedCells)
-        .reduce((acc, selectedCells) => acc.concat(selectedCells), []);
-
-      const existingCell = rectGroup.select(`#${cellId}`);
-
-      if (!existingCell.empty()) {
-        // Check if the cell is assigned
-        const isAssigned = assignedCells.some(
-          (cell) => cell.x === x && cell.y === y
-        );
-
-        // If the cell is already highlighted, and not being assigned, remove the highlight
-        if (!isAssigned) {
-          existingCell.remove();
-          updatedSelectedCells = updatedSelectedCells.filter(
-            (cell) => cell.x !== x || cell.y !== y
-          );
-        }
-      } else {
-        updatedSelectedCells = [...updatedSelectedCells, { x, y }];
-        rectGroup
-          .append("rect")
-          .attr("id", cellId)
-          .attr("x", xScale(x))
-          .attr("y", yScale(y))
-          .attr("width", xScale(1) - xScale(0))
-          .attr("height", yScale(1) - yScale(0))
-          .attr("fill", "#CBC568")
-          .attr("opacity", 0.5);
-      }
-      setSelectedCells((prevSelectedCells) => updatedSelectedCells);
+      handleAreaSelect({
+        event,
+        xScale,
+        yScale,
+        assignments,
+        rectGroup,
+        updatedSelectedCells,
+        setUpdatedSelectedCells,
+      });
     });
   }, [gridSize, selectedOption, assignments, startPlotting]);
 
+  // update local selected cells
+  useEffect(() => {
+    console.log(updatedSelectedCells, "180808");
+    setSelectedCells(updatedSelectedCells);
+  }, [updatedSelectedCells]);
+
   // size toggle bar handler
-  // const handelSizeChange = debounce((value) => {
-  //   console.log(e, '8080808')
-  //   setGridSize(+value);
-  // }, 500);
+  const handelSizeChange = (e) => setGridSize(+e.target.value);
 
   return (
     <div className="interactive-grid">
@@ -392,7 +152,7 @@ const InteractiveGrid = ({
         min="1"
         max="50"
         value={gridSize}
-        onChange={(e) => setGridSize(+e.target.value)}
+        onChange={handelSizeChange}
       />
       <div
         id="tooltip"
---
+++ src/components/interactiveGridMethods.js
@@ -0,0 +1,311 @@
+import * as d3 from "d3";
+// background
+export const createImageGroup = (svg, width, height, backgroundImageUrl) => {
+  const imageGroup = svg.append("g");
+  imageGroup
+    .append("image")
+    .attr("xlink:href", backgroundImageUrl)
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", width)
+    .attr("height", height)
+    .attr("pointer-events", "none");
+  imageGroup.raise();
+};
+// Overlay
+export const createOverlayGroup = (svg, width, height) => {
+  const overlayGroup = svg.append("g");
+  overlayGroup
+    .append("rect")
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", width)
+    .attr("height", height)
+    .attr("fill", "white")
+    .attr("opacity", 0.5);
+  overlayGroup.raise();
+};
+// Grid
+export const createGripGroup = ({
+  svg,
+  width,
+  height,
+  xScale,
+  yScale,
+  gridSize,
+}) => {
+  const gridGroup = svg.append("g");
+  gridGroup.selectAll(".grid-line").remove();
+  const gridLines = d3.range(0, gridSize + 1);
+  // Vertical lines
+  gridGroup
+    .selectAll(".grid-line-vertical")
+    .data(gridLines)
+    .enter()
+    .append("line")
+    .attr("class", "grid-line grid-line-vertical")
+    .attr("x1", (d) => xScale(d))
+    .attr("x2", (d) => xScale(d))
+    .attr("y1", 0)
+    .attr("y2", height)
+    .attr("stroke", "grey")
+    .attr("opacity", 0.6)
+    .attr("stroke-width", 1);
+
+  // Horizontal lines
+  gridGroup
+    .selectAll(".grid-line-horizontal")
+    .data(gridLines)
+    .enter()
+    .append("line")
+    .attr("class", "grid-line grid-line-horizontal")
+    .attr("x1", 0)
+    .attr("x2", width)
+    .attr("y1", (d) => yScale(d))
+    .attr("y2", (d) => yScale(d))
+    .attr("stroke", "grey")
+    .attr("opacity", 0.6)
+    .attr("stroke-width", 1);
+};
+// AssignedCells
+export const createAssignedCells = ({
+  rectGroup,
+  assignments,
+  xScale,
+  yScale,
+}) => {
+  assignments.forEach((assignment) => {
+    const zoneName = assignment.zoneName;
+
+    let firstCellInAssignment;
+    assignment.selectedCells.forEach((cell, index) => {
+      const cellId = `cell-${cell.x}-${cell.y}`;
+      const existingCell = rectGroup.select(`#${cellId}`);
+
+      if (existingCell.empty()) {
+        rectGroup
+          .append("rect")
+          .attr("id", cellId)
+          .attr("x", xScale(cell.x))
+          .attr("y", yScale(cell.y))
+          .attr("width", xScale(1) - xScale(0))
+          .attr("height", yScale(1) - yScale(0))
+          .attr("fill", "#CBC568")
+          .attr("opacity", 0.5);
+      }
+
+      if (index === 0) {
+        firstCellInAssignment = cell;
+      }
+    });
+
+    // Add the surrounding square
+    rectGroup
+      .append("rect")
+      .attr("class", "zone-name-tag-bg")
+      .attr("x", xScale(firstCellInAssignment.x) + xScale(1) + 2 - 2) // Adjust the position as needed
+      .attr("y", yScale(firstCellInAssignment.y) - 20) // Adjust the position as needed
+      .attr("width", zoneName.length * 6 * 2) // Adjust the width based on the length of the zone name
+      .attr("height", 25) // Adjust the height as needed
+      .attr("fill", "#DCDCDC")
+      .attr("opacity", 0.8);
+
+    // Add the zone name tag
+    rectGroup
+      .append("text")
+      .attr("class", "zone-name-tag")
+      .attr("x", xScale(firstCellInAssignment.x) + xScale(1) + 2) // Adjust the position as needed
+      .attr("y", yScale(firstCellInAssignment.y) - 2) // Adjust the position as needed
+      .attr("font-size", "24px")
+      .attr("font-weight", "bold")
+      .attr("fill", "black")
+      .attr("opacity", 0.8)
+      .text(zoneName);
+  });
+};
+// current data points
+export const plotDataPoint = ({
+  rectGroup,
+  dataPoint,
+  deviceColors,
+  height,
+}) => {
+  if (!dataPoint) return;
+  const x = dataPoint?.projected_norm_x;
+  const y = dataPoint?.projected_norm_y;
+  const isInDeviceColors = deviceColors[dataPoint.ClientMacAddr];
+  const staffOrDevice = dataPoint["Staff/Device"];
+
+  // Selected fixed device but the state has not occurred yet
+  const isFixedNormal = isInDeviceColors && dataPoint.localtime < 1568121986000;
+  // Selected fixed device and the state has occurred
+  const isFixedWithState =
+    isInDeviceColors && dataPoint.localtime >= 1568121986000;
+  // is staff but not specifically for this device
+  const isStaff = dataPoint["Staff ID"] !== "nan";
+
+  let color, size, alpha, strokeWidth, stroke;
+
+  if (isFixedNormal) {
+    color = "#0096FF";
+    size = 8;
+    alpha = 1;
+    strokeWidth = 3;
+    stroke = "black";
+  } else if (isFixedWithState) {
+    color = "red";
+    size = 10;
+    alpha = 1;
+    strokeWidth = 2;
+    stroke = "black";
+  } else if (isStaff) {
+    color = "orange";
+    size = 7;
+    alpha = 1;
+    strokeWidth = 0;
+    stroke = "transparent";
+  } else {
+    // Default case
+    color = "black";
+    size = 5;
+    alpha = 0.5;
+    strokeWidth = 0;
+    stroke = "transparent";
+  }
+
+  // Show tooltip
+  const showTooltip = (event) => {
+    const tooltip = document.getElementById("tooltip");
+    const content = `
+        <strong>${staffOrDevice}</strong><br/>
+        ClientMacAddr: ${dataPoint.ClientMacAddr}`;
+
+    tooltip.innerHTML = content;
+    tooltip.style.left = event.pageX + 10 + "px";
+    tooltip.style.top = event.pageY + 10 + "px";
+    tooltip.style.display = "block";
+  };
+
+  // Hide tooltip
+  const hideTooltip = () => {
+    const tooltip = document.getElementById("tooltip");
+    tooltip.style.display = "none";
+  };
+
+  // Plot the data point
+  rectGroup
+    .append("circle")
+    .attr("cx", x)
+    .attr("cy", height - y)
+    .attr("r", size)
+    .attr("fill", color)
+    .attr("opacity", alpha)
+    .attr("pointer-events", "all")
+    .style("stroke", stroke)
+    .style("stroke-width", strokeWidth)
+    .on("mouseover", showTooltip)
+    .on("mousemove", showTooltip)
+    .on("mouseout", hideTooltip);
+};
+
+// handle area select
+export const handleAreaSelect = ({
+  event,
+  xScale,
+  yScale,
+  assignments,
+  rectGroup,
+  updatedSelectedCells,
+  setUpdatedSelectedCells,
+}) => {
+  const coords = d3.pointer(event);
+
+  const x = Math.floor(xScale.invert(coords[0]));
+  const y = Math.floor(yScale.invert(coords[1]));
+
+  const cellId = `cell-${x}-${y}`;
+  const assignedCells = assignments
+    .map((assignment) => assignment.selectedCells)
+    .reduce((acc, selectedCells) => acc.concat(selectedCells), []);
+
+  const existingCell = rectGroup.select(`#${cellId}`);
+
+  if (!existingCell.empty()) {
+    // Check if the cell is assigned
+    const isAssigned = assignedCells.some(
+      (cell) => cell.x === x && cell.y === y
+    );
+
+    // If the cell is already highlighted, and not being assigned, remove the highlight
+    if (!isAssigned) {
+      existingCell.remove();
+      setUpdatedSelectedCells(
+        updatedSelectedCells.filter((cell) => cell.x !== x || cell.y !== y)
+      );
+    }
+  } else {
+    console.log(updatedSelectedCells, { x, y }, "180808");
+    setUpdatedSelectedCells((prevUpdatedSelectedCells) => [
+      ...prevUpdatedSelectedCells,
+      { x, y },
+    ]);
+
+    rectGroup
+      .append("rect")
+      .attr("id", cellId)
+      .attr("x", xScale(x))
+      .attr("y", yScale(y))
+      .attr("width", xScale(1) - xScale(0))
+      .attr("height", yScale(1) - yScale(0))
+      .attr("fill", "#CBC568")
+      .attr("opacity", 0.5);
+  }
+};
+
+// auto play time gap
+export const TIME_GAP = 500;
+
+// draw current data list
+export const drawCurData = ({
+  currentIndex,
+  rectGroup,
+  dataPoints,
+  setCurrentDataPoints,
+  onDataPointsForTime,
+  assignments,
+  deviceColors,
+  height,
+}) => {
+  const startTime = dataPoints[currentIndex + 1].localtime;
+  // clear previous data points
+  rectGroup.selectAll("circle").remove();
+  // get the current time window
+  const currentTime = startTime + currentIndex * TIME_GAP;
+  const currentData = dataPoints.filter(
+    (d) => d.localtime >= currentTime && d.localtime < currentTime + TIME_GAP
+  );
+  setCurrentDataPoints((prevCurrentDataPoints) => currentData);
+  // Check if the current time is 13:26:26
+  if (currentData.length !== 0 && currentData[0].localtime === 1568121986000) {
+    onDataPointsForTime(currentData, assignments);
+  }
+  currentData.forEach((dataPoint) => {
+    plotDataPoint({ rectGroup, dataPoint, deviceColors, height });
+    // Update the timer display
+    const timerDisplay = document.getElementById("timer");
+    const date = new Date(dataPoint.localtime);
+    const londonTimeOptions = {
+      timeZone: "UTC",
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+    const londonTime = date.toLocaleString("en-GB", londonTimeOptions);
+    const fractionalSeconds = date.getMilliseconds();
+
+    timerDisplay.textContent = `${londonTime}.${fractionalSeconds ? "5" : "0"}`;
+  });
+};
